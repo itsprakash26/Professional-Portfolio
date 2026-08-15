0f790a52aee02294f7d055a0b1286eb979a6e4ee
om prakash choudhary <omprakeshchaudhary01@gmail.com>
2026-08-16 00:28:57 +0530

    Fix CV download path

--- Portfolio/src/components/Hero/HeroButtons.jsx
+++ Portfolio/src/components/Hero/HeroButtons.jsx
@@ -4,24 +4,26 @@ function HeroButtons() {
       behavior: "smooth",
     });
   };
+
   const downloadCV = () => {
-    window.open("./Omprakash.pdf", "_blank");
+    window.open("/Omprakash.pdf", "_blank");
   };
+
   return (
     <div className="flex gap-5 mt-10">
-
-      <button 
-      onClick={goToProjects}
-      className="bg-violet-600 hover:bg-violet-700 px-8 py-4 rounded-lg">
+      <button
+        onClick={goToProjects}
+        className="bg-violet-600 hover:bg-violet-700 px-8 py-4 rounded-lg"
+      >
         View My Work
       </button>
 
-      <button 
-      onClick={downloadCV}
-      className="border border-gray-600 px-8 py-4 rounded-lg hover:border-violet-500">
+      <button
+        onClick={downloadCV}
+        className="border border-gray-600 px-8 py-4 rounded-lg hover:border-violet-500"
+      >
         Download CV
       </button>
-
     </div>
   );
 }
